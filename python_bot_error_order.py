b169dd41202f1b0b1416f02944419a696d139b60
Tuomas <38554334+tuomaskupila@users.noreply.github.com>
2020-08-10 10:45:53 +0300

    Update python_bot_error_order.py

--- python_bot_error_order.py
+++ python_bot_error_order.py
@@ -1,7 +1,7 @@
 import sys
  
-sys.stdout.write("STDOUT: Hello World!\n")
-sys.stderr.write("STDERR: Darkness rises!\n")
+sys.stdout.write("STDOUT: Where am I?\n")
+sys.stderr.write("STDERR: Welcome!\n")
 sys.stdout.write("STDOUT: Who was that?\n")
-sys.stderr.write("STDERR: ...\n")
+sys.stderr.write("STDERR: Error-man from space!\n")
 exit(42)
